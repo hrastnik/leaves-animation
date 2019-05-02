441b5fd72b61bfab6516ae219855bd20a6a7a9ce
Mateo Hrastnik <hrastnikmateo@gmail.com>
2019-05-02 16:11:55 +0200

    added basic fall animation

---
+++ components/Leaves.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="leaves__container">
+    <img class="leaves__image" src="@/assets/leaf.png" alt="leaf" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.leaves {
+  &__container {
+    position: relative;
+  }
+
+  &__image {
+    position: absolute;
+
+    left: 50vw;
+
+    min-width: 50px;
+    width: 16vw;
+    max-width: 300px;
+    animation: falling 8s linear 0s infinite;
+  }
+}
+
+@keyframes falling {
+  from {
+    transform: translateY(-100%);
+  }
+  to {
+    transform: translateY(calc(100vh + 100%));
+  }
+}
+</style>
+
---
+++ pages/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <section class="container">
+    <Leaves />
+  </section>
+</template>
+
+<script>
+import Leaves from "~/components/Leaves.vue";
+
+export default {
+  components: {
+    Leaves
+  }
+};
+</script>
+
+<style scoped>
+.container {
+  min-height: 100vh;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
